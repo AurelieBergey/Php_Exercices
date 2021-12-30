436d6d216cfb3ab95a4b47b390f9b761e2cb2b51
Aurelie.Bergey <aurelie.bergey@gmail.com>
2021-12-30 14:23:01 +0100

    📦 NEW: initial commit

---
+++ Exercice.php
@@ -0,0 +1,59 @@
+<?php
+
+// echo "afficher le prix sur le web"; 
+
+   
+//    $prix_ht = 50;
+//    $tva = 20;
+//    $prix_ttc = $prix_ht + 50*$tva*0.01;
+//    echo 'Le prix TTC du produit est de '.$prix_ttc.' €.';
+
+// echo "Condition de budget";
+
+
+//    $budget = 200.235;
+//    $achat =200.23;
+
+//    if( $achat <= $budget ){
+//       echo ("Vous pouvez l'acheter");
+//    }else{
+//       echo ("Va bosser");
+//    }
+
+//    $condition = ($budget >= $achat);      // façon ternaire
+//    echo ($condition ? "Youpi ! Je vais pouvoir acheter mon mac !" : 'Zut ! Le mac est trop chère, t\'facon apple c\'est nul...');
+
+//Exercice 
+// faire un carrée complet parfait avec les étoiles
+
+// for($i=0; $i<5; $i++){
+   
+//    echo "*";
+   
+
+//    for($j=0 ; $j<10 ; $j++){
+//       echo "*";
+      
+//    }
+//    echo "</br>";
+// }
+      
+//Exercice 
+// faire un carrée vide parfait avec les étoiles
+
+// 
+
+// Faire un triangle
+
+// for($i=0; $i<=10; $i++){
+   
+//       echo "*";
+//       for($j=$i; $j<10; $j++){
+//          echo "*";
+//       }
+     
+//       echo "</br>";
+//    }
+
+ ?>
+ 
